d7b875670be672e0d0b7987c0509cf9285dfed4a
Jack Thompson <60582130+randock233@users.noreply.github.com>
2023-12-30 08:54:03 +0000

    Update cl_nets.lua
    QOL

--- lua/autorun/client/cl_nets.lua
+++ lua/autorun/client/cl_nets.lua
@@ -1,27 +1,26 @@
-net.Receive( "Karma:ChatPrint", function( len, ply )
-    local msg,color = net.ReadString() , net.ReadColor()
-    chat.AddText( Color( 45, 0, 255), "[Karma] ", color, msg )
+net.Receive("Karma:ChatPrint", function(len, ply)
+    local msg, color = net.ReadString(), net.ReadColor()
+    chat.AddText(Color(45, 0, 255), "[Karma] ", color, msg)
 end)
 
-surface.CreateFont( "realthugv2", {
+surface.CreateFont("realthugv2", {
     font = "Arial",
     size = 32,
     weight = 600,
     antialias = true,
     shadow = false
-} )
+})
 
 local function lgbtq()
     return HSVToColor((CurTime() * 100) % 360, 1, 1)
 end
 
-
 local function DrawHUD()
-    local 🤓 = LocalPlayer()
-    if not IsValid(🤓) then return end
-    if not 🤓:Alive() then return end
+    local player = LocalPlayer()
+    if not IsValid(player) then return end
+    if not player:Alive() then return end
     
-    local karma = 🤓:GetNWInt("karma") or 0
+    local karma = player:GetNWInt("karma") or 0
     local w, h = ScrW(), ScrH()
     local box_w, box_h = 200, 50
     local x, y = 10, 10
@@ -40,58 +39,55 @@ local function DrawHUD()
     draw.SimpleText("Rep: " .. karma, "realthugv2", x + box_w/2, y + box_h/2, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 end
 
-
-net.Receive( "SendFirst.💜", function( 💜, __ )
-    local 💜💜💜💜 = net.ReadInt( 32 )
-    local 🤓 = LocalPlayer()
-    --print('El net'..💜💜💜💜)
-    LocalPlayer().GotPayload = true
-    🤓:SetNWInt( "karma", 💜💜💜💜 )
-    hook.Remove( "HUDPaint", "DrawHUD:Lucas" )
-    timer.Simple(0 ,  function()
+net.Receive("SendFirstKarma", function(len, player)
+    local karmaValue = net.ReadInt(32)
+    local localPlayer = LocalPlayer()
+    localPlayer.GotPayload = true
+    localPlayer:SetNWInt("karma", karmaValue)
+    hook.Remove("HUDPaint", "DrawHUD:Lucas")
+    timer.Simple(0, function()
         hook.Add("HUDPaint", "DrawHUD:Lucas", function()
             if LocalPlayer().DontShowKarma then return end
-            if !LocalPlayer().GotPayload then return end
+            if not LocalPlayer().GotPayload then return end
             DrawHUD()
         end)  
     end)
 end)
 
+hook.Add("OnPlayerChat", "karma:admincmds", function(player, text)
+    if player ~= LocalPlayer() then return end
+    local lowerText = string.lower(text)
+    local splitText = string.Explode(" ", lowerText)
 
-hook.Add("OnPlayerChat", "karma:admincmds", function(🤓, 📚)
-    if 🤓 ~= LocalPlayer() then return end
-    📚📚 = string.lower(📚)
-    📚📚📚 = string.Explode(" ", 📚📚)
-
-    if 📚📚📚[1] == '/wipekarma' then
-        net.Start("👍")
-        net.WriteString(📚📚📚[2])
+    if splitText[1] == '/wipekarma' then
+        net.Start("receiveData")
+        net.WriteString(splitText[2])
         net.WriteString('wipe:Solo')
         net.WriteInt(0, 32)
         net.SendToServer()
 
         return true
-    elseif 📚📚📚[1] == '/wipekarma_all' then
-        net.Start("👍")
+    elseif splitText[1] == '/wipekarma_all' then
+        net.Start("receiveData")
         net.WriteString('RealThug')
         net.WriteString('wipe:Global')
         net.WriteInt(0, 32)
         net.SendToServer()
 
         return true
-    elseif 📚📚📚[1] == '/addkarma' then
-        net.Start("👍")
-        net.WriteString(📚📚📚[2])
+    elseif splitText[1] == '/addkarma' then
+        net.Start("receiveData")
+        net.WriteString(splitText[2])
         net.WriteString('add:karma')
-        net.WriteInt(📚📚📚[3], 32)
+        net.WriteInt(tonumber(splitText[3]), 32)
         net.SendToServer()
 
         return true
-    elseif 📚📚📚[1] == '/removekarma' then
-        net.Start("👍")
-        net.WriteString(📚📚📚[2])
+    elseif splitText[1] == '/removekarma' then
+        net.Start("receiveData")
+        net.WriteString(splitText[2])
         net.WriteString('remove:karma')
-        net.WriteInt(📚📚📚[3], 32)
+        net.WriteInt(tonumber(splitText[3]), 32)
         net.SendToServer()
 
         return true
